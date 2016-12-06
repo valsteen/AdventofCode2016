f26a0f24fdb6d69eca83d5f4d5246dc0b445b427
Vincent Alsteen <vincent.alsteen@gmail.com>
2016-12-06 23:04:58 +0100

    5 part 2

--- 5.py
+++ 5.py
@@ -19,5 +19,32 @@ class Advent5(object):
         self.input = input
 
 
+class Advent5Part2(Advent5):
+    def find(self):
+        found = 0
+        self.result = [None] * self.iterations
+
+        while found < self.iterations:
+            hash = md5("{}{}".format(self.input, self.index)).hexdigest()
+            if hash.startswith("00000"):
+                try:
+                    position = int(hash[5])
+                except ValueError:
+                    pass
+                else:
+                    if position < self.iterations:
+                        character = hash[6]
+
+                        if self.result[position] is None:
+                            self.result[position] = character
+                            print self.result
+                            found += 1
+
+            self.index += 1
+
+        return "".join(self.result)
+
+
 #print Advent5("abc", 8).find()
-print Advent5("uqwqemis", 8).find()
+print Advent5Part2("abc", 8).find()
+print Advent5Part2("uqwqemis", 8).find()
